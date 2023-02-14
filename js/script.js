065e29fe98db9151a4d261517abf909143b9cdb1
ruaa <ruaa.reda99@gmail.com>
2023-02-14 19:01:29 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,106 +1,105 @@
-$("#owl-demo").owlCarousel({
-  navigation: true,
-});
-$(".one-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  dots: false,
-  responsive: {
-    0: {
-      items: 2,
+$(window).on("load", function () {
+  $(".one-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      780: {
+        items: 3,
+      },
+      1100: {
+        items: 4,
+      },
     },
-    780: {
-      items: 3,
+  });
+  $("#two-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      780: {
+        items: 2,
+      },
+      1100: {
+        items: 3,
+      },
     },
-    1100: {
-      items: 4,
-    },
-  },
-});
-$("#two-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  dots: false,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    780: {
-      items: 2,
-    },
-    1100: {
-      items: 3,
-    },
-  },
-});
+  });
 
-$("#computers-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  dots: false,
-  responsive: {
-    0: {
-      items: 1,
+  $("#computers-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      780: {
+        items: 1,
+      },
+      1100: {
+        items: 2,
+      },
     },
-    780: {
-      items: 1,
-    },
-    1100: {
-      items: 2,
-    },
-  },
-});
+  });
 
-$("#logos-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  dots: false,
-  responsive: {
-    0: {
-      items: 2,
+  $("#logos-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      780: {
+        items: 4,
+      },
+      1100: {
+        items: 6,
+      },
     },
-    780: {
-      items: 4,
-    },
-    1100: {
-      items: 6,
-    },
-  },
-});
+  });
 
-$("#hotdeals-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  dots: false,
-  responsive: {
-    0: {
-      items: 1,
+  $("#hotdeals-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      780: {
+        items: 2,
+      },
+      992: {
+        items: 1,
+      },
     },
-    780: {
-      items: 2,
-    },
-    992: {
-      items: 1,
-    },
-  },
-});
+  });
 
-$(".btn-sidebar").click(function () {
-  $(this).toggleClass("click");
-  $(".sidebar").toggleClass("show");
-});
+  $(".btn-sidebar").click(function () {
+    $(this).toggleClass("click");
+    $(".sidebar").toggleClass("show");
+  });
 
-$(".sidebar ul li a").click(function () {
-  var id = $(this).attr("id");
-  $("nav ul li ul.item-show-" + id).toggleClass("show");
-  $("nav ul li #" + id + " span").toggleClass("rotate");
-});
+  $(".sidebar ul li a").click(function () {
+    var id = $(this).attr("id");
+    $("nav ul li ul.item-show-" + id).toggleClass("show");
+    $("nav ul li #" + id + " span").toggleClass("rotate");
+  });
 
-$("nav ul li").click(function () {
-  $(this).addClass("active").siblings().removeClass("active");
+  $("nav ul li").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+  });
 });
